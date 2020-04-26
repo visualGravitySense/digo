f354b16a7986be9c8e25a1d48e3d5a65255dbc3d
visualGravitySense <helikeel@gmail.com>
2020-04-26 23:56:21 +0300

    Parser Connection Commit

--- pars/views.py
+++ pars/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from .utils import *
+from .models import *
 def home(request):
+    jobs = djinni()
+    city = City.objects.get(name='Lviv')
+
     return render(request, 'base.html')
