9c9772e700df3af5584ed81a0326b1cf2b52f4b2
visualGravitySense <helikeel@gmail.com>
2020-04-27 12:29:36 +0300

    views commit: djinni parse

--- pars/views.py
+++ pars/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .utils import *
 from .models import *
+
 def home(request):
     jobs = djinni()
     city = City.objects.get(name='Lviv')
+    speciality = Speciality.objects.get(name='Python')
+    v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
+    url_list = [i['url'] for i in v]
+    for job in jobs:
+        if job['url'] not in url_list
+            vacancy = Vacancy(city=city.id, speciality=speciality.id, url=job['url']
+                                title=job['title'], description=job['descript'], company=job['company'])
+            vacancy.save()
 
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'jobs': jobs})
