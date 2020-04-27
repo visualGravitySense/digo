644a9f0a3a92503642056a53d108c2aa520b0c22
visualGravitySense <helikeel@gmail.com>
2020-04-27 16:24:08 +0300

    views commit

--- pars/views.py
+++ pars/views.py
@@ -9,9 +9,9 @@ def home(request):
     v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
     url_list = [i['url'] for i in v]
     for job in jobs:
-        if job['url'] not in url_list
-            vacancy = Vacancy(city=city.id, speciality=speciality.id, url=job['url']
-                                title=job['title'], description=job['descript'], company=job['company'])
+        if job['href'] not in url_list:
+            vacancy = Vacancy(city=city.id, speciality=speciality.id, url=job['href'],
+                            title=job['title'], description=job['descript'], company=job['company'])
             vacancy.save()
 
     return render(request, 'base.html', {'jobs': jobs})
