f4d1fd779b8b6feeb9678c13068b1c3da37027d2
visualGravitySense <helikeel@gmail.com>
2020-05-09 00:43:33 +0300

    Django bugs fix 1.

--- pars/urls.py
+++ pars/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('administrator/', admin.site.urls),
     path('home/', home),
 ]
--- pars/views.py
+++ pars/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from scraping.utils import *
-from scraping.models import *
+#from scraping.utils import *
+#from scraping.models import *
 
 def home(request):
-    jobs = []
-    jobs.extend(djinni())
-    jobs.extend(rabota())
-    jobs.extend(work())
+    return render(request, 'base.html')
+
+#    jobs = []
+#    jobs.extend(djinni())
+#    jobs.extend(rabota())
+#    jobs.extend(work())
+'''
     city = City.objects.get(name='Lviv')
     speciality = Speciality.objects.get(name='Python')
     v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
@@ -14,7 +17,10 @@ def home(request):
     for job in jobs:
         if job['href'] not in url_list:
             vacancy = Vacancy(city=city, speciality=speciality, url=job['href'],
-                            title=job['title'], description=job['descript'], company=job['company'])
+                            title=job['title'], description=job['descript'],
+                            company=job['company'])
             vacancy.save()
 
-    return render(request, 'base.html', {'jobs': jobs})
+#    return render(request, 'base.html', {'jobs': jobs})
+
+'''
