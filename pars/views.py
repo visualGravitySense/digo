79270cd8ecef2346b40618b46b674bac2712694f
visualGravitySense <helikeel@gmail.com>
2020-05-10 19:05:19 +0300

    django bugs fixing 1/3

--- pars/views.py
+++ pars/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-#from scraping.utils import *
-#from scraping.models import *
+from scraping.utils import *
+from scraping.models import *
 
 def home(request):
-    return render(request, 'base.html')
+    jobs = djinni()
+    city = City.objects.get(name='Kyiv')
+    speciality = Speciality.objects.get(name='Python')
+    v = Vacancy.objects.filter(city=city, speciality=speciality).values('url')
+    url_list = [i['url'] for i in v]
+    for job in jobs:
+        if job['href'] not in url_list:
+            vacancy = Vacancy(city=city, speciality=speciality,
+            url=job['href'], title=job['title'], description=job['descript'],
+            company=job['company'])
+    vacancy.save()
+    return render(request, 'base.html', {'jobs': jobs})
 
 #    jobs = []
 #    jobs.extend(djinni())
@@ -11,16 +22,6 @@ def home(request):
 #    jobs.extend(work())
 
 '''
-    city = City.objects.get(name='Lviv')
-    speciality = Speciality.objects.get(name='Python')
-    v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
-    url_list = [i['url'] for i in v]
-    for job in jobs:
-        if job['href'] not in url_list:
-            vacancy = Vacancy(city=city, speciality=speciality, url=job['href'],
-                            title=job['title'], description=job['descript'],
-                            company=job['company'])
-            vacancy.save()
 
 #    return render(request, 'base.html', {'jobs': jobs})
 
