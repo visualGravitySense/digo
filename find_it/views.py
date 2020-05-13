a1b29648f46d85cb8cd610151d800dc67e925597
visualGravitySense <helikeel@gmail.com>
2020-05-13 16:20:53 +0300

    django models vol  2

--- find_it/views.py
+++ find_it/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from scraping.utils import *
+from scraping.models import *
 
 def home(request):
-    return render(request, 'base.html')
+    jobs = djinni()
+    city = City.objects.get(name='Lviv')
+    speciality = Speciality.objects.get(name='Python')
+    v = Vacancy.objects.filter(city=City, speciality=Speciality).values('url')
+    url_list = [i['url'] for i in v]
+    for job in jobs:
+        if job['href'] not in url_list:
+            vacancy = Vacancy(city=City, speciality=Speciality, url=job['href'],
+                                title=job['title'], desvription=job['descript'],
+                                company=job['company'])
+            vacancy.save()
+
+    return render(request, 'base.html', {'jobs': jobs})
