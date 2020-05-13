c34434fd1e34d3044c4347a6d8c2ca3e2e98a306
visualGravitySense <helikeel@gmail.com>
2020-05-13 17:41:07 +0300

    django 3 parsers connected

--- find_it/views.py
+++ find_it/views.py
@@ -3,14 +3,17 @@ from scraping.utils import *
 from scraping.models import *
 
 def home(request):
-    jobs = djinni()
+    jobs = []
+    jobs.extend(djinni())
+    jobs.extend(rabota())
+    jobs.extend(work())
     city = City.objects.get(name='Lviv')
     speciality = Speciality.objects.get(name='Python')
     v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
     url_list = [i['url'] for i in v]
     for job in jobs:
         if job['href'] not in url_list:
-            vacancy = Vacancy(city=City, speciality=Speciality, url=job['href'],
+            vacancy = Vacancy(city=city, speciality=speciality, url=job['href'],
                                 title=job['title'], description=job['descript'],
                                 company=job['company'])
             vacancy.save()
--- scraping/utils.py
+++ scraping/utils.py
@@ -37,6 +37,110 @@ def djinni():
                             'company': "'No name'"})
     return jobs
 
+def rabota():
+    session = requests.Session()
+    headers = {'User-Agent': 'Safari/12.1.2 (MacOS Mojave 10.14.6) Gecko/20100101 Firefox/47.0',
+                'Accept':'text/html,application/xhtml+xml,application/xhtml;q=0.9,*/*;q=0.8'
+            }
+    base_url = 'https://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=Python'
+
+    domain = 'https://rabota.ua'
+    jobs = []
+    urls = []
+    #yesterday = datetime.date.today()-datetime.timedelta(1)
+    #one_day_ago = yesterday.strftime('%d.%m.%Y')
+    #base_url = base_url + one_day_ago
+    urls.append(base_url)
+
+    req = session.get(base_url, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, "html.parser")
+        pagination = bsObj.find('dl', attrs={'id': 'ctl00_content_ctl00_gridList_ctl23_pagerInnerTable'})
+        if pagination:
+            pages = pagination.find_all('a', attrs={'class': 'f-always-blue'})
+            for page in pages:
+                urls.append(domain + page['href'])
+
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
+        if req.status_code == 200:
+            bsObj = BS(req.content, "html.parser")
+            table = bsObj.find('table', attrs={'id': 'ctl00_content_ctl00_gridList'})
+            if table:
+                tr_list = bsObj.find_all('tr', attrs={'id': True})
+                for tr in tr_list:
+                    p = tr.find('p', attrs={'class': 'card-title'})
+                    title = p.a.text
+                    href = p.a['href']
+                    short = 'No description'
+                    company = "No name"
+                    logo = tr.find('p', attrs={'class': 'company-name'})
+                    if logo:
+                        company = logo.a.text
+                    p = tr.find('div', attrs={'class': 'card-description'})
+                    if p:
+                        short = p.text
+                    jobs.append({'href': domain + href,
+                                'title': title,
+                                'descript': short,
+                                'company': company})
+
+    return jobs
+
+def work():
+    session = requests.Session()
+
+    '''
+    url = 'https://www.work.ua/ru/jobs-kyiv-python/'
+    '''
+    base_url = 'https://www.work.ua/ru/jobs-kyiv-python/'
+
+    domain = 'https://www.work.ua'
+    jobs = []
+    urls = []
+    urls.append(base_url)
+    req = session.get(base_url, headers=headers)
+
+    '''
+    req = session.get(base_url, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, "html.parser")
+        pagination = bsObj.find('ul', attrs={'class': 'pagination'})
+        if pagination:
+            pages = pagination.find_all('li', attrs={'class': False})
+            for page in pages:
+                urls.append(domain + page.a['href'])
+
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
+        if req.status_code == 200:
+            bsObj = BS(req.content, "html.parser")
+
+    '''
+    for url in urls:
+        time.sleep(2)
+
+        if req.status_code == 200:
+            bsObj = BS(req.content, "html.parser")
+            div_list = bsObj.find_all('div', attrs={'class': 'job-link'})
+            for div in div_list:
+                title = div.find('h2')
+                href = title.a['href']
+                short = div.p.text
+                company = "No name"
+                logo = div.find('img')
+                if logo:
+                    company = logo['alt']
+                jobs.append({'href': domain + href,
+                    'title': title.text,
+                    'descript': short,
+                    'company': company})
+
+
+    return jobs
+
 
 '''
 import requests
