bd76630bad51de0da3757d82d8512ff2ed78e85b
visualGravitySense <helikeel@gmail.com>
2020-05-13 16:42:44 +0300

    django models bugs fix 2a

--- find_it/views.py
+++ find_it/views.py
@@ -6,12 +6,12 @@ def home(request):
     jobs = djinni()
     city = City.objects.get(name='Lviv')
     speciality = Speciality.objects.get(name='Python')
-    v = Vacancy.objects.filter(city=City, speciality=Speciality).values('url')
+    v = Vacancy.objects.filter(city=city.id, speciality=speciality.id).values('url')
     url_list = [i['url'] for i in v]
     for job in jobs:
         if job['href'] not in url_list:
             vacancy = Vacancy(city=City, speciality=Speciality, url=job['href'],
-                                title=job['title'], desvription=job['descript'],
+                                title=job['title'], description=job['descript'],
                                 company=job['company'])
             vacancy.save()
 
