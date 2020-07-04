8566b1a23813be18660ec661a3934d61d5dc0e6f
Dmitri Gornakov <helikeel@gmail.com>
2020-07-04 14:50:12 +0300

    Add files via upload

--- djinni.py
+++ djinni.py
@@ -2,15 +2,13 @@ import requests
 from bs4 import BeautifulSoup as BS
 import codecs
 import time
-
 session = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-        }
+           'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
+base_url = 'https://djinni.co/jobs/?primary_keyword=Python&location=%D0%9A%D0%B8%D0%B5%D0%B2'
 
-base_url = 'https://djinni.co/jobs/?primary_keyword=Python&page=1&location=Киев'
-
-domain = 'https://djinni.co/'
+domain = 'https://djinni.co'
 jobs = []
 urls = []
 urls.append(base_url)
@@ -27,14 +25,17 @@ for url in urls:
             title = div.a.text
             href = div.a['href']
             short = 'No description'
+            # company = "No name"
             descr = li.find('div', attrs={'class': 'list-jobs__description'})
             if descr:
-                short = descr.text
+                short = descr.p.text
             jobs.append({'href': domain + href,
-                        'title': title,
+                        'title': title, 
                         'descript': short,
-                        'company': "'No name'"})
-
+                        'company': "No name"})
+    
+    # print(div.find('p', attrs={'class': 'overflow'}).text)
+# data = bsObj.prettify()#.encode('utf8')
 template = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>'
 end = '</body></html>'
 content = '<h2> Djinni.co</h2>'
@@ -42,6 +43,6 @@ for job in jobs:
     content += '<a href="{href}" target="_blank">{title}</a><br/><p>{descript}</p><p>{company}</p><br/>'.format(**job)
     content += '<hr/><br/><br/>'
 data = template + content + end
-handle = codecs.open('djinni.html', "w", 'utf-8')
+handle = codecs.open('jobs.html', "w", 'utf-8')
 handle.write(str(data))
-handle.close()
+handle.close() 
--- dou.py
+++ dou.py
@@ -2,12 +2,11 @@ import requests
 from bs4 import BeautifulSoup as BS
 import codecs
 import time
-
 session = requests.Session()
-headers = {'User-Agent': 'Safari/12.1.2 (MacOS Mojave 10.14.6) Gecko/20100101 Firefox/47.0',
-            'Accept':'text/html,application/xhtml+xml,application/xhtml;q=0.9,*/*;q=0.8'
-        }
-base_url = 'https://jobs.dou.ua/vacancies/?category=Python'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+           'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
+base_url = 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'
 
 
 jobs = []
@@ -35,19 +34,19 @@ for url in urls:
                 if descr:
                     short = descr.text
                 jobs.append({'href': href,
-                            'title': title,
+                            'title': title, 
                             'descript': short,
                             'company': company})
-
-
-
-template = '<!doctype html><html lang="en"><head><meta charset="utf-8"><head><body>'
+    
+    # print(div.find('p', attrs={'class': 'overflow'}).text)
+# data = bsObj.prettify()#.encode('utf8')
+template = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>'
 end = '</body></html>'
-content = '<h2> Dou</h2>'
+content = '<h2> Dou.ua</h2>'
 for job in jobs:
     content += '<a href="{href}" target="_blank">{title}</a><br/><p>{descript}</p><p>{company}</p><br/>'.format(**job)
     content += '<hr/><br/><br/>'
 data = template + content + end
-handle = codecs.open('dou.html', "w", "utf-8")
+handle = codecs.open('jobs.html', "w", 'utf-8')
 handle.write(str(data))
-handle.close()
+handle.close() 
