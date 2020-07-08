3c5e91ab8416d2bc7d88d8250c42257a2d341fac
Dmitri Gornakov <helikeel@gmail.com>
2020-07-08 22:03:47 +0300

    Add files via upload

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from .models import *
 
-class VacancyAdmin(admin.ModelAdmin):
+# Register your models here.
+admin.site.register(City)
+admin.site.register(Vacancy)
+admin.site.register(Specialty)
+
+"""
+from django.contrib import admin
+from .models import *
+
+class VacancyAdnim(admin.ModelAdmin):
     class Meta:
         model = Vacancy
-    list_display = ('title', 'url', 'city', 'speciality', 'timestamp')
+    list_display = ('title', 'url', 'city', 'specialty', 'timestamp')
 
 admin.site.register(City)
-admin.site.register(Vacancy, VacancyAdmin)
-admin.site.register(Speciality)
+admin.site.register(Vacancy, VacancyAdnim)
+admin.site.register(Specialty)
 admin.site.register(Site)
 admin.site.register(Url)
+admin.site.register(Error)
+"""
--- scraping/utils.py
+++ scraping/utils.py
@@ -3,219 +3,76 @@ from bs4 import BeautifulSoup as BS
 import codecs
 import time
 import datetime
+import random
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-        }
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 
-def djinni(base_url):
+def djinni():
+    session = requests.Session()
+    base_url ='https://djinni.co/jobs/?title_only=True&primary_keyword=Python&location=Киев'
+
+    domain = 'https://djinni.co'
     jobs = []
     urls = []
-    errors = []
-    if base_url:
-        session = requests.Session()
-        domain = 'https://djinni.co'
-        urls.append(base_url)
-        # urls.append(base_url+'&page=2')
+#    errors = []
+    urls.append(base_url)
+    urls.append(base_url+'&page=2')
 
-        for url in urls:
-            time.sleep(2)
-            nmb = random.randint(0, 2)
-            req = session.get(url, headers=headers[nmb])
-            if req.status_code == 200:
-                bsObj = BS(req.content, "html.parser")
-                li_list = bsObj.find_all('li', attrs={'class': 'list-jobs__item'})
-                if li_list:
-                    for li in li_list:
-                        div = li.find('div', attrs={'class': 'list-jobs__title'})
-                        title = div.a.text
-                        href = div.a['href']
-                        short = 'No description'
-                        # company = "No name"
-                        descr = li.find('div', attrs={'class': 'list-jobs__description'})
-                        if descr:
-                            short = descr.text
-                        jobs.append({'href': domain + href,
-                                    'title': title,
-                                    'descript': short,
-                                    'company': "No name"})
-                else:
-                    errors.append({'href': url,
-                                'title': 'The page is empty'})
-            else:
-                errors.append({'href': url,
-                                'title': 'Page do not response'})
-    return jobs, errors
-
-def rabota(base_url):
-    jobs = []
-    urls = []
-    errors = []
-    if base_url:
-        session = requests.Session()
-        domain = 'https://rabota.ua'
-        yesterday = datetime.date.today()-datetime.timedelta(1)
-        one_day_ago = yesterday.strftime('%d.%m.%Y')
-        base_url = base_url + one_day_ago
-        urls.append(base_url)
-
-        nmb = random.randint(0, 2)
-        req = session.get(base_url, headers=headers[nmb])
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
         if req.status_code == 200:
             bsObj = BS(req.content, "html.parser")
-            pagination = bsObj.find('dl',
-                    attrs={'id':
-                    'ctl00_content_vacancyList_gridList_ctl23_pagerInnerTable'
-                        })# ctl00_content_VacancyListAws_gridList_ctl23_pagerInnerTable
-            if pagination:
-                pages = pagination.find_all('a', attrs={'class': 'f-always-blue'})
-                for page in pages:
-                    urls.append(domain + page['href'])
-        else:
-            errors.append({'href': base_url,
-                                'title': 'Page do not response'})
-        # print(urls)
-        # legal_words = legal_words.split(' ') if legal_words else None
-        for url in urls:
-            time.sleep(2)
-            nmb = random.randint(0, 2)
-            req = session.get(url, headers=headers[nmb])
-            if req.status_code == 200:
-                bsObj = BS(req.content, "html.parser")
-                new_jobs_found = not(bsObj.find('div',
-                                attrs={'class': 'f-vacancylist-newnotfound'}))
-                if new_jobs_found:
-                    table = bsObj.find('table',
-                            attrs={'id': 'ctl00_content_vacancyList_gridList'})
-                            # ctl00_content_VacancyListAws_gridList
-                    #       ctl00_content_vacancyList_vacList_gridList
-                    if table:
-                        tr_list = bsObj.find_all('tr', attrs={'id': True})
-                        for tr in tr_list:
-                            div = tr.find('div',
-                                          attrs={'class': 'card-body'})
-                            h3 = div.find('p',
-                                    attrs={'class': 'card-title'})
-                            title = h3.a.text
-                            href = h3.a['href']
-                            short = 'No description'
-                            company = "No name"
-                            company_name = tr.find('p',
-                                        attrs={'class': 'company-name'})
-                            if company_name:
-                                _title = company_name.a['title']
-                                _text = company_name.a.text
-                                _title = _title if _title else _text
-                                if _title:
-                                    company = _title
-                            # logo = tr.find('p',
-                            #             attrs={'class': 'f-vacancylist-companyname'})
-                            # if logo:
-                            #     try:
-                            #         company = logo.text
-                            #     except:
-                            #         pass
-                            # p = tr.find('p',
-                            #             attrs={'class': 'f-vacancylist-shortdescr'})
-                            p = div.find('div',
-                                         attrs={'class': 'card-description'})
-                            if p:
-                                short = p.text
-
-                            jobs.append({'href': domain + href,
-                                        'title': title,
-                                        'descript': short,
-                                        'company': company})
+            li_list = bsObj.find_all('li', attrs={'class': 'list-jobs__item'})
+            for li in li_list:
+                div = li.find('div', attrs={'class': 'list-jobs__title'})
+                title = div.a.text
+                href = div.a['href']
+                short = 'No description'
+                # company = "No name"
+                descr = li.find('div', attrs={'class': 'list-jobs__description'})
+                if descr:
+                    short = descr.text
+                jobs.append({'href': domain + href,
+                            'title': title,
+                            'descript': short,
+                            'company': "No name"})
 
     return jobs
 
-def work(base_url):
+def rabota():
+    session = requests.Session()
+    base_url = 'https://www.cv.ee/toopakkumised/harjumaa/infotehnoloogia/'
+    domain = 'https://www.cv.ee'
     jobs = []
     urls = []
-    errors = []
-    if base_url:
-        session = requests.Session()
-        domain = 'https://www.work.ua'
-        urls.append(base_url)
-        nmb = random.randint(0, 2)
-        req = session.get(base_url, headers=headers[nmb])
+    urls.append(base_url)
+    req = session.get(base_url, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, "html.parser")
+        pagination = bsObj.find('div', attrs={'id': 'page-pagination'})
+        if pagination:
+            pages = pagination.find_all('li', attrs={'class': False})
+            for page in pages:
+                urls.append(domain + page.a['href'])
+
+    for url in urls:
+        time.sleep(2)
+        req = session.get(url, headers=headers)
         if req.status_code == 200:
             bsObj = BS(req.content, "html.parser")
-            pagination = bsObj.find('ul', attrs={'class': 'pagination'})
-            if pagination:
-                pages = pagination.find_all('li', attrs={'class': False})
-                for page in pages:
-                    urls.append(domain + page.a['href'])
-        else:
-            errors.append({'href': base_url,
-                                'title': 'Page do not respose'})
-        for url in urls:
-            time.sleep(2)
-            nmb = random.randint(0, 2)
-            req = session.get(url, headers=headers[nmb])
-            if req.status_code == 200:
-                bsObj = BS(req.content, "html.parser")
-                div_list = bsObj.find_all('div', attrs={'class': 'job-link'})
-                if div_list:
-                    for div in div_list:
-                        title = div.find('h2')
-                        href = title.a['href']
-                        short = div.p.text
-                        company = "No name"
-                        logo = div.find('img')
-                        if logo:
-                            company = logo['alt']
-                        jobs.append({'href': domain + href,
-                                    'title': title.text,
-                                    'descript': short,
-                                    'company': company})
-                else:
-                    errors.append({'href': url,
-                                'title': 'The page is empty'})
-            else:
-                errors.append({'href': url,
-                                'title': 'Page do not response'})
+            div_list = bsObj.find_all('div', attrs={'class': 'offer_primary'})
+            for div in div_list:
+                title = div.find('h2')
+                href = title.a['href']
+                company = "No name"
+                logo = div.find('li', {"class": "offer-company"})
+                if logo:
+                    company = logo('a')
+                jobs.append({'href': domain + href,
+                             'title': title.text,
+                             'descript': "No name",
+                             'company': company})
 
-    return jobs, errors
-
-def dou(base_url):
-
-    jobs = []
-    urls = []
-    errors = []
-    if base_url:
-        urls.append(base_url)
-        session = requests.Session()
-
-        for url in urls:
-            time.sleep(2)
-            nmb = random.randint(0, 2)
-            req = session.get(url, headers=headers[nmb])
-            if req.status_code == 200:
-                bsObj = BS(req.content, "html.parser")
-                div = bsObj.find('div', attrs={'id': 'vacancyListId'})
-                if div:
-                    li_list = bsObj.find_all('li', attrs={'class': 'l-vacancy'})
-                    for li in li_list:
-                        a = li.find('a', attrs={'class': 'vt'})
-                        title = a.text
-                        href = a['href']
-                        short = 'No description'
-                        company = "No name"
-                        a_company = li.find('a', attrs={'class': 'company'})
-                        if a_company:
-                            company = a_company.text
-                        descr = li.find('div', attrs={'class': 'sh-info'})
-                        if descr:
-                            short = descr.text
-                        jobs.append({'href': href,
-                                    'title': title,
-                                    'descript': short,
-                                    'company': company})
-                else:
-                    errors.append({'href': url,
-                                'title': 'The page is empty'})
-            else:
-                errors.append({'href': url,
-                                'title': 'Page do not response'})
-    return jobs, errors
+    return jobs
