5ab3f181ef34e4115b77bbbf67abb114e8c256d8
Dmitri Gornakov <helikeel@gmail.com>
2020-07-09 11:25:49 +0300

    Add files via upload

--- find_it/views.py
+++ find_it/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
+from django.db import IntegrityError
 from scraping.utils import *
 from scraping.models import *
-from django.db import IntegrityError
-
 
 def home(request):
+    jobs = []
+    jobs.extend(djinni())
+    jobs.extend(rabota())
     city = City.objects.get(name='Киев')
     specialty = Specialty.objects.get(name='Python')
-    url_qs = Url.objects.filter(city=city, specialty=specialty)
-    site = Site.objects.all()
-    url_w = url_qs.get(site=site.get(name='Work.ua')).url_address
-    url_dj = url_qs.get(site=site.get(name='Djinni.co')).url_address
-    url_r = url_qs.get(site=site.get(name='Rabota.ua')).url_address
-    url_dou = url_qs.get(site=site.get(name='Dou.ua')).url_address
-    jobs = []
-    jobs.extend(djinni(url_dj))
-    jobs.extend(rabota(url_r))
-    jobs.extend(work(url_w))
-    jobs.extend(dou(url_dou))
+#    url_qs = Url.objects.filter(city=city, specialty=specialty)
+#    site = Site.objects.all()
+#    url_w = url_qs.get(site=site.get(name='Work.ua')).url_address
+#    url_dj = url_qs.get(site=site.get(name='Djinni.co')).url_address
+#    url_r = url_qs.get(site=site.get(name='Rabota.ua')).url_address
+#    url_dou = url_qs.get(site=site.get(name='Dou.ua')).url_address
+
+#    jobs.extend(work(url_w))
+#    jobs.extend(dou(url_dou))
 
     # v = Vacancy.objects.filter(city=city.id, specialty=specialty.id).values('url')
     # url_list = [i['url'] for i in v]
