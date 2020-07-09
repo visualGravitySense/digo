385f1d556ea819448766c8a77b64ca495fb3e538
Dmitri Gornakov <helikeel@gmail.com>
2020-07-09 13:38:33 +0300

    Add files via upload
    getting income data for scraping automation

--- scraping/admin.py
+++ scraping/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class VacancyAdnim(admin.ModelAdmin):
+    class Meta:
+        model = Vacancy
+    list_display = ('title', 'url', 'city', 'specialty', 'timestamp')
+
 admin.site.register(City)
 admin.site.register(Vacancy)
 admin.site.register(Specialty)
+admin.site.register(Site)
+admin.site.register(Url)
 
 """
 from django.contrib import admin
